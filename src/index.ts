60ca33cd145c8d5d3c1f83d1f0926b64b04ea1b7
Uday Patel <pateluday9725@gmail.com>
2026-03-16 23:33:13 +0530

    meke new repo

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import { WebSocketServer ,WebSocket } from 'ws'
+interface messageType {
+    username : string,
+    message : string
+}
+const wss = new WebSocketServer ({port :8080})
+    const AllSocket :WebSocket[]=[]
+    const messages:messageType[]  = []
+wss.on("connection",function(socket){
+    AllSocket.push(socket)
+
+    console.log("user Connected")
+    socket.on("message",(data)=>{
+        const parsed :messageType =JSON.parse(data.toString())
+        messages.push(parsed)
+       for(let i=0 ; i < AllSocket.length ; i++){
+        const  s = AllSocket[i]
+        s!.send(JSON.stringify(parsed))
+       }
+    })
+})
